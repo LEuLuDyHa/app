4716c19a95df965974db14f4da9012ee2ec1b645
Luis Romero Rodriguez <luis.romerorodriguez@epfl.ch>
2023-02-28 21:09:02 +0100

    Include all necessary changes to use Google Maps API. Creation of a placeholder activity using a composable google maps function (equivalent to a fragment).

---
+++ app/src/main/java/com/github/leuludyha/ibdb/GoogleMapsActivity.kt
@@ -0,0 +1,62 @@
+package com.github.leuludyha.ibdb
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.github.leuludyha.ibdb.ui.theme.IBDBTheme
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
+
+class GoogleMapsActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            IBDBTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    GoogleMaps()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun GoogleMaps() {
+    val singapore = LatLng(1.35, 103.87)
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(singapore, 10f)
+    }
+    GoogleMap(
+        modifier = Modifier.fillMaxSize(),
+        cameraPositionState = cameraPositionState
+    ) {
+        Marker(
+            state = MarkerState(position = singapore),
+            title = "Singapore",
+            snippet = "Marker in Singapore"
+        )
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview3() {
+    IBDBTheme {
+        GoogleMaps()
+    }
+}
