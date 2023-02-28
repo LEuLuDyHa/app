6b839031f836ed66e048ce5d6d0d7723e3df5e6f
Luis Romero Rodriguez <luis.romerorodriguez@epfl.ch>
2023-02-28 22:45:22 +0100

    Add new testing class for the GoogleMapsActivity.

---
+++ app/src/androidTest/java/com/github/leuludyha/ibdb/GoogleMapsActivityTest.kt
@@ -0,0 +1,17 @@
+package com.github.leuludyha.ibdb
+
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+
+import org.junit.Rule
+import org.junit.Test
+
+class GoogleMapsActivityTest {
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<GoogleMapsActivity>()
+
+    @Test
+    fun clickingOnEPFLMarkerShowsPreviouslyInvisibleDelimiterPolygon() {
+        //TODO: I don't see how to do this actually...
+    }
+}
