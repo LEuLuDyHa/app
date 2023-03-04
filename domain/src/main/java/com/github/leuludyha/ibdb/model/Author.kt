3eea22d8796007313a4ba52c04e620d57cecdd4d
wollfie <leowolllff@gmail.com>
2023-03-04 08:55:25 +0100

    Propose architecture for the project & Implement User, Book and Author base objects
    Consists of the app (view) layer, to which we add two libraries : data & domain (repositories and model) This enables to separate the different layer to which we might want to work quite well

---
+++ domain/src/main/java/com/github/leuludyha/ibdb/model/Author.kt
@@ -0,0 +1,8 @@
+package com.github.leuludyha.ibdb.model
+
+// We might want to improve this in the future but I figured this is the minimum we need
+data class Author(
+    val id: String,
+    val firstName: String,
+    val lastName: String,
+)
---
+++ domain/src/main/java/com/github/leuludyha/ibdb/model/Book.kt
@@ -0,0 +1,36 @@
+package com.github.leuludyha.ibdb.model
+
+import java.util.*
+
+data class Book(
+    // Id from which we can find the rest of info on the book
+    val id: String,
+    // I think it is reasonable to always store the title, it might make it easier to debug also
+    val title: String,
+    // Also there is a subtitle
+    val subtitle: String,
+    // The isbn just to identify the book further if need be
+    val isbn: String,
+    // And the rest of book information which might or might not be present locally in the object,
+    // in which case we have to fetch it in some database
+    val info: Optional<BookInfo>,
+)
+
+// Use parameter jscmd=data to get these in API call
+data class BookInfo(
+    val nbOfPages: Int,
+    val languages: List<String>,
+    val authors: List<Author>,
+    val publishers: List<String>,
+    val subjects: List<String>,
+    val notes: String,
+    val coverUrls: BookCoverUrls,
+    // Something called "By statement" in the API which might be useful to us
+    val byStatement: String,
+)
+
+data class BookCoverUrls(
+    val small: String,
+    val medium: String,
+    val large: String,
+)
