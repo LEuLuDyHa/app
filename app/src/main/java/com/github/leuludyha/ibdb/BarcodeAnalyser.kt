3527d9434b25d7ddcfa63dd872d408b5d898322f
Luis Romero Rodriguez <luis.romerorodriguez@epfl.ch>
2023-03-10 11:18:48 +0100

    Add Activity to scan barcodes

---
+++ app/src/main/java/com/github/leuludyha/ibdb/BarcodeAnalyser.kt
@@ -0,0 +1,46 @@
+import android.annotation.SuppressLint
+import android.util.Log
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions
+import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.barcode.common.Barcode
+import com.google.mlkit.vision.common.InputImage
+
+/*
+    This code Analyzer allows to search for (exclusively) ISBN barcodes on an image.
+    It has been built following this tutorial: https://developers.google.com/ml-kit/vision/barcode-scanning/android
+ */
+@SuppressLint("UnsafeOptInUsageError")
+class BarcodeAnalyser(
+    private val onISBNBarcodeDetected: (barcodes: List<Barcode>) -> Unit,
+): ImageAnalysis.Analyzer {
+
+    override fun analyze(imageProxy: ImageProxy) {
+        Log.println(Log.INFO, "test", "I am analyzing an image!")
+        val mediaImage = imageProxy.image
+        if(mediaImage != null) {
+            val image = InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
+
+            //EAN13 is the format ISBN barcodes are encoded in, no need to look for the others.
+            val options = BarcodeScannerOptions.Builder()
+                .setBarcodeFormats(Barcode.FORMAT_EAN_13)
+                .build()
+
+            val scanner = BarcodeScanning.getClient(options)
+
+            scanner.process(image)
+                .addOnSuccessListener { barcodes ->
+                    Log.println(Log.INFO, "test", "I found some barcodes!!")
+                    onISBNBarcodeDetected(barcodes.filter {
+                        barcode -> barcode.valueType == Barcode.TYPE_ISBN
+                    })
+                }
+                .addOnFailureListener {
+                    exception -> Log.d("TAG", "BarcodeAnalyser: Something went wrong $exception")
+                }.addOnCompleteListener {
+                    mediaImage.close()
+                }
+        }
+    }
+}
---
+++ app/src/main/java/com/github/leuludyha/ibdb/BarcodeScanningActivity.kt
@@ -0,0 +1,143 @@
+package com.github.leuludyha.ibdb
+
+import BarcodeAnalyser
+import android.os.Bundle
+import android.util.Log
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberPermissionState
+import com.google.common.util.concurrent.ListenableFuture
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+/*
+    This class allows to scan a barcode.
+    It has been built by following this tutorial: https://developer.android.com/codelabs/camerax-getting-started
+ */
+class BarcodeScanningActivity : ComponentActivity() {
+
+    @OptIn(ExperimentalPermissionsApi::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Surface {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Bottom
+                ) {
+                    Spacer(modifier = Modifier.height(10.dp))
+
+                    val cameraPermissionState =
+                        rememberPermissionState(permission = android.Manifest.permission.CAMERA)
+
+                    Button(
+                        onClick = {
+                            cameraPermissionState.launchPermissionRequest()
+                        }
+                    ) {
+                        Text(text = "Camera Permission")
+                    }
+
+                    Spacer(modifier = Modifier.height(10.dp))
+
+                    CameraPreview()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CameraPreview() {
+    val context = LocalContext.current
+    val lifecycleOwner = LocalLifecycleOwner.current
+    var preview by remember { mutableStateOf<Preview?>(null) }
+    val barCodeVal = remember { mutableStateOf("") }
+
+    AndroidView(
+        factory = { AndroidViewContext ->
+            PreviewView(AndroidViewContext).apply {
+                this.scaleType = PreviewView.ScaleType.FILL_CENTER
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                )
+                implementationMode = PreviewView.ImplementationMode.COMPATIBLE
+            }
+        },
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(50.dp),
+        update = { previewView ->
+
+            val cameraExecutor: ExecutorService = Executors.newSingleThreadExecutor()
+            val cameraProviderFuture: ListenableFuture<ProcessCameraProvider> =
+                ProcessCameraProvider.getInstance(context)
+
+            cameraProviderFuture.addListener({
+                // Used to bind the lifecycle of cameras to the lifecycle owner
+                val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+                //Select back camera as default
+                val cameraSelector : CameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+                // Preview
+                preview = Preview.Builder().build().also {
+                    it.setSurfaceProvider(previewView.surfaceProvider)
+                }
+
+                //This is our created class that will only look for ISBN barcodes in the image
+                //(and execute the callback just defined for all the found barcodes
+                val barcodeAnalyser = BarcodeAnalyser { barcodes ->
+                    barcodes.forEach { barcode ->
+                        barcode.rawValue?.let { barcodeValue ->
+                            barCodeVal.value = barcodeValue
+                            Toast.makeText(context, barcodeValue, Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+                //This ImageAnalysis object takes care of feeding input images from the camera to
+                //our created barcodeAnalyser.
+                val imageAnalysis: ImageAnalysis = ImageAnalysis.Builder()
+                    .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                    .build()
+                    .also {
+                        it.setAnalyzer(cameraExecutor, barcodeAnalyser)
+                    }
+
+                try {
+                    //Here we are binding all elements just created to the lifetime of the cameraProvider.
+                    cameraProvider.unbindAll()
+                    cameraProvider.bindToLifecycle(
+                        lifecycleOwner,
+                        cameraSelector,
+                        preview,
+                        imageAnalysis
+                    )
+                } catch (e: Exception) {
+                    Log.d("TAG", "CameraPreview: ${e.localizedMessage}")
+                }
+            }, ContextCompat.getMainExecutor(context))
+        }
+    )
+}
