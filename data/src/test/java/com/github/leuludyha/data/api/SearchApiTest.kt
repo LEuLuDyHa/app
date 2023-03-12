994c1f1f8bf865007c35f6d5dace2eadc2a27e51
Dylan <dylan.vairoli@gmail.com>
2023-03-12 11:28:45 +0100

    Adapt test for new RemoteDataSource interface

--- data/src/test/java/com/github/leuludyha/data/api/SearchApiTest.kt
+++ data/src/test/java/com/github/leuludyha/data/api/SearchApiTest.kt
@@ -6,7 +6,7 @@ import com.github.leuludyha.data.repository.datasourceImpl.LibraryRemoteDataSour
 import com.github.leuludyha.domain.model.Document
 import com.github.leuludyha.domain.model.Search
 import com.github.leuludyha.domain.repository.LibraryRepository
-import com.github.leuludyha.domain.util.FileReader
+import com.github.leuludyha.data.FileReader
 import com.google.common.truth.Truth.assertThat
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
@@ -15,7 +15,6 @@ import okhttp3.mockwebserver.MockWebServer
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
-import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.net.HttpURLConnection
@@ -41,7 +40,7 @@ class SearchApiTest {
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
-    fun twoDocsTest(dataProvider: suspend (String) -> Response<Search>) = runTest {
+    fun twoDocsTest(dataProvider: suspend (String) -> Search) = runTest {
 
         val json = FileReader.readResourceFromFile(this.javaClass.classLoader!!, "2docs_search.json")
 
@@ -68,55 +67,25 @@ class SearchApiTest {
             authorKeys = listOf("TestAuthorKey1")
         )
 
-        val actualResponse = dataProvider("test").body()
+        val actualResponse = dataProvider("test")
         assertThat(actualResponse).isNotNull()
-        assertThat(actualResponse!!.resultsCount).isEqualTo(2)
+        assertThat(actualResponse.resultsCount).isEqualTo(2)
         assertThat(actualResponse.resultsCount).isEqualTo(actualResponse.documents.size)
         assertThat(actualResponse.documents).isEqualTo(listOf(expectedDoc0, expectedDoc1))
     }
 
     @Test
     fun `for multiple documents found, api must return all documents with http code 200`() =
-        twoDocsTest { s -> api.search(s) }
+        twoDocsTest { s -> api.search(s).body()!! }
 
     @Test
     fun `for multiple documents found, RemoteDataSource must return all documents with http code 200`() =
-        twoDocsTest { s -> remoteDataSource.search(s) }
+        twoDocsTest { s -> remoteDataSource.search(s).data!! }
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    fun `for multiple documents found, Repository must return all documents with http code 200`() = runTest {
-
-        val json = FileReader.readResourceFromFile(this.javaClass.classLoader!!, "2docs_search.json")
-
-        val expectedResponse = MockResponse()
-            .setResponseCode(HttpURLConnection.HTTP_OK)
-            .setBody(json)
-        mockWebServer.enqueue(expectedResponse)
-
-        val expectedDoc0 = Document(
-            title = "TestBook0",
-            coverId = 0,
-            authorNames = listOf("TestAuthor0"),
-            firstPublishYear = 2023,
-            key = "TestKey0",
-            authorKeys = listOf("TestAuthorKey0")
-        )
-
-        val expectedDoc1 = Document(
-            title = "TestBook1",
-            coverId = 1,
-            authorNames = listOf("TestAuthor1"),
-            firstPublishYear = 2023,
-            key = "TestKey1",
-            authorKeys = listOf("TestAuthorKey1")
-        )
-
-        val actualResponse = repository.search("test")
-        assertThat(actualResponse.data).isNotNull()
-        assertThat(actualResponse.data!!.resultsCount).isEqualTo(2)
-        assertThat(actualResponse.data!!.documents).isEqualTo(listOf(expectedDoc0, expectedDoc1))
-    }
+    fun `for multiple documents found, Repository must return all documents with http code 200`() =
+        twoDocsTest { s -> repository.search(s).data!! }
 
     @After
     fun tearDown() {
