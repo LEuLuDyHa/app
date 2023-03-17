45e345ba4093276d4937dbf43cdf060664e04fef
Eugene Flesselle <eugene@flesselle.net>
2023-03-17 07:39:40 +0100

    Add AuthRepository and SignInUseCases to di

--- app/src/main/java/com/github/leuludyha/ibdb/di/UseCaseModule.kt
+++ app/src/main/java/com/github/leuludyha/ibdb/di/UseCaseModule.kt
@@ -1,7 +1,11 @@
 package com.github.leuludyha.ibdb.di
 
+import com.github.leuludyha.domain.repository.AuthRepository
 import com.github.leuludyha.domain.repository.LibraryRepository
 import com.github.leuludyha.domain.useCase.SearchUseCase
+import com.github.leuludyha.domain.useCase.auth.signin.FirebaseSignInUseCase
+import com.github.leuludyha.domain.useCase.auth.signin.OneTapSignInUseCase
+import com.github.leuludyha.domain.useCase.auth.signin.SignInUseCases
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -10,7 +14,16 @@ import dagger.hilt.components.SingletonComponent
 @Module
 @InstallIn(SingletonComponent::class)
 object UseCaseModule {
+
     @Provides
     fun provideSearchUseCase(libraryRepository: LibraryRepository) =
        SearchUseCase(libraryRepository)
+
+    @Provides
+    fun provideSignInUseCases(authRepository: AuthRepository) =
+        SignInUseCases(
+            oneTapSignInUseCase = OneTapSignInUseCase(authRepository),
+            firebaseSignInUseCase = FirebaseSignInUseCase(authRepository)
+        )
+
 }
