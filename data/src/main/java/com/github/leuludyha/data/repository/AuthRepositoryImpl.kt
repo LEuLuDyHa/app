c1035130ae06cad08f1b1a7e0cbc3645dc432ab9
Eugene Flesselle <eugene@flesselle.net>
2023-03-18 13:59:38 +0100

    Add AuthRepository implementation of firebaseSignInWithGoogle

--- data/src/main/java/com/github/leuludyha/data/repository/AuthRepositoryImpl.kt
+++ data/src/main/java/com/github/leuludyha/data/repository/AuthRepositoryImpl.kt
@@ -1,12 +1,14 @@
 package com.github.leuludyha.data.repository
 
+import com.github.leuludyha.domain.model.Result
 import com.github.leuludyha.domain.repository.AuthRepository
 import com.github.leuludyha.domain.repository.OneTapSignInResponse
 import com.github.leuludyha.domain.repository.SignInWithGoogleResponse
 import com.google.android.gms.auth.api.identity.BeginSignInRequest
 import com.google.android.gms.auth.api.identity.SignInClient
 import com.google.firebase.auth.AuthCredential
-import com.github.leuludyha.domain.model.Result
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.tasks.await
 
 // TODO ? data sources for :
@@ -18,7 +20,11 @@ class AuthRepositoryImpl(
     private var oneTapClient: SignInClient,
     private var signInRequest: BeginSignInRequest,
     private var signUpRequest: BeginSignInRequest,
+    private val auth: FirebaseAuth,
+    private val db: FirebaseFirestore
 ): AuthRepository {
+
+    override val currentUser = auth.currentUser
 
     override suspend fun oneTapSignInWithGoogle(): OneTapSignInResponse {
         return try {
@@ -34,8 +40,20 @@ class AuthRepositoryImpl(
         }
     }
 
-    override suspend fun firebaseSignInWithGoogle(googleCredential: AuthCredential): SignInWithGoogleResponse {
-        TODO("Not yet implemented")
+    override suspend fun firebaseSignInWithGoogle(
+        googleCredential: AuthCredential
+    ): SignInWithGoogleResponse {
+        return try {
+            val authResult = auth.signInWithCredential(googleCredential).await()
+            if (authResult.additionalUserInfo?.isNewUser == true) {
+                auth.currentUser?.apply {
+                    // TODO("add user to firestore db")
+                }
+            }
+            Result.Success(true)
+        } catch (e: Exception) {
+            Result.Error(e.message.toString())
+        }
     }
 
 }
--- domain/src/main/java/com/github/leuludyha/domain/repository/AuthRepository.kt
+++ domain/src/main/java/com/github/leuludyha/domain/repository/AuthRepository.kt
@@ -3,11 +3,14 @@ package com.github.leuludyha.domain.repository
 import com.google.android.gms.auth.api.identity.BeginSignInResult
 import com.google.firebase.auth.AuthCredential
 import com.github.leuludyha.domain.model.Result
+import com.google.firebase.auth.FirebaseUser
 
 typealias OneTapSignInResponse = Result<BeginSignInResult>
 typealias SignInWithGoogleResponse = Result<Boolean>
 
 interface AuthRepository {
+
+    val currentUser: FirebaseUser?
 
     suspend fun oneTapSignInWithGoogle(): OneTapSignInResponse
 
