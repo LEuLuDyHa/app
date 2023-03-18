d09cb56d3e8ad5869eb961c651a4fa0d002f116d
Eugene Flesselle <eugene@flesselle.net>
2023-03-18 11:54:43 +0100

    Add AuthRepository implementation of oneTapSignInWithGoogle

---
+++ data/src/main/java/com/github/leuludyha/data/repository/AuthRepositoryImpl.kt
@@ -0,0 +1,41 @@
+package com.github.leuludyha.data.repository
+
+import com.github.leuludyha.domain.repository.AuthRepository
+import com.github.leuludyha.domain.repository.OneTapSignInResponse
+import com.github.leuludyha.domain.repository.SignInWithGoogleResponse
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.SignInClient
+import com.google.firebase.auth.AuthCredential
+import com.github.leuludyha.domain.model.Result
+import kotlinx.coroutines.tasks.await
+
+// TODO ? data sources for :
+//      - OneTap: SignInClient, BeginSignInRequest, BeginSignUpRequest
+//      - Firebase: FirebaseAuth, FirebaseFirestore
+//  but cannot abstract over the OneTapSignInResponse and AuthCredential types
+
+class AuthRepositoryImpl(
+    private var oneTapClient: SignInClient,
+    private var signInRequest: BeginSignInRequest,
+    private var signUpRequest: BeginSignInRequest,
+): AuthRepository {
+
+    override suspend fun oneTapSignInWithGoogle(): OneTapSignInResponse {
+        return try {
+            val signInResult = oneTapClient.beginSignIn(signInRequest).await()
+            Result.Success(signInResult)
+        } catch (e: Exception) {
+            try {
+                val signUpResult = oneTapClient.beginSignIn(signUpRequest).await()
+                Result.Success(signUpResult)
+            } catch (e: Exception) {
+                Result.Error(e.message.toString())
+            }
+        }
+    }
+
+    override suspend fun firebaseSignInWithGoogle(googleCredential: AuthCredential): SignInWithGoogleResponse {
+        TODO("Not yet implemented")
+    }
+
+}
