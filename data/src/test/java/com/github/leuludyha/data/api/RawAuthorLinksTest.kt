2ba2829c2eb4f767d54bafad6a8029fe7118a7f9
Dylan <dylan.vairoli@gmail.com>
2023-03-27 12:22:44 +0200

    Add more raw tests

---
+++ data/src/test/java/com/github/leuludyha/data/api/RawAuthorLinksTest.kt
@@ -0,0 +1,21 @@
+package com.github.leuludyha.data.api
+
+import com.github.leuludyha.data.RequiringLibraryApiTest
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+class RawAuthorLinksTest: RequiringLibraryApiTest() {
+
+    @Test
+    fun `Fields are properly gotten`() {
+        val authorLinks = RawAuthorLinks(
+            authorKey = "/authors/OL34184A",
+            prevKey = "x",
+            nextKey = "x"
+        )
+
+        assertThat(authorLinks.authorKey).isEqualTo( "/authors/OL34184A")
+        assertThat(authorLinks.prevKey).isEqualTo( "x")
+        assertThat(authorLinks.nextKey).isEqualTo( "x")
+    }
+}
---
+++ data/src/test/java/com/github/leuludyha/data/api/RawSearchTest.kt
@@ -0,0 +1,75 @@
+package com.github.leuludyha.data.api
+
+import com.github.leuludyha.data.RequiringLibraryApiTest
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+
+class RawSearchTest: RequiringLibraryApiTest() {
+
+    @Test
+    fun `Fields are properly gotten`() {
+        val doc = RawDocument(
+            coverId = 0,
+            title = "Awesome book",
+            authorNames = listOf("Awesome guy"),
+            firstPublishYear = 0,
+            key = "/work/key",
+            authorIds = listOf("/authors/authorId"),
+            editionIds = listOf("key")
+        )
+        val search = RawSearch(
+            documents = listOf(doc),
+            error = "x"
+        )
+
+        assertThat(search.documents).isEqualTo(listOf(doc))
+        assertThat(search.error).isEqualTo("x")
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun `toModel returns expected works`() { runTest {
+        mockWebServer.enqueue(workResponse)
+
+        val search = RawSearch(
+            documents = listOf(mockRawDocument),
+            error = null,
+        )
+
+        val works = search.toModel(libraryApi)
+
+        assertThat(works).isEqualTo(listOf(mockWork))
+    }}
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun `toModel fails on invalid key`() { runTest {
+        mockWebServer.enqueue(workResponse)
+
+        val search = RawSearch(
+            documents = listOf(mockRawDocument.copy(key = "/work/invalid")),
+            error = null,
+        )
+
+        val works = search.toModel(libraryApi)
+
+        assertThat(works).isEmpty()
+    }}
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun `toModel fails on error`() { runTest {
+        mockWebServer.enqueue(workResponse)
+
+        val search = RawSearch(
+            documents = listOf(mockRawDocument.copy(key = "/work/invalid")),
+            error = "error",
+        )
+
+        val works = search.toModel(libraryApi)
+
+        assertThat(works).isEmpty()
+    }}
+}
