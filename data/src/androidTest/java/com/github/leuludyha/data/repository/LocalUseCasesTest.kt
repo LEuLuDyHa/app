213ed06e8fc6e20c74ce3cdf61f53e4fb27dd3e3
Dylan <dylan.vairoli@gmail.com>
2023-04-10 10:49:19 +0200

    Add tests for save `UseCase`s

--- data/src/androidTest/java/com/github/leuludyha/data/repository/LocalUseCasesTest.kt
+++ data/src/androidTest/java/com/github/leuludyha/data/repository/LocalUseCasesTest.kt
@@ -4,11 +4,13 @@ import com.github.leuludyha.data.RequiringLibraryRepositoryTest
 import com.github.leuludyha.data.TestUtils.author1
 import com.github.leuludyha.data.TestUtils.edition1
 import com.github.leuludyha.data.TestUtils.work1
-import com.github.leuludyha.domain.useCase.GetAuthorLocallyUseCase
-import com.github.leuludyha.domain.useCase.GetEditionLocallyUseCase
-import com.github.leuludyha.domain.useCase.GetWorkLocallyUseCase
+import com.github.leuludyha.domain.model.library.Author
+import com.github.leuludyha.domain.model.library.Edition
+import com.github.leuludyha.domain.model.library.Work
+import com.github.leuludyha.domain.useCase.*
 import com.google.common.truth.Truth.assertThat
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.runBlocking
 import org.junit.Test
 
@@ -38,5 +40,52 @@ class LocalUseCasesTest: RequiringLibraryRepositoryTest() {
     fun getAuthorLocallyUseCaseGivesCorrectResult() = runBlocking {
         assertThat(GetAuthorLocallyUseCase(libraryRepository)(author1.authorId).first())
             .isEqualTo(author1.toModel(libraryDao))
+    }
+
+    @Test
+    fun saveWorkLocallyUseCaseCorrectlySavesNewAuthor() = runBlocking {
+        val testWork = Work(
+            id = "TestWork",
+            title = "title",
+            editions = flowOf(),
+            authors = flowOf(),
+            covers = flowOf(),
+            subjects = flowOf()
+        )
+        SaveWorkLocallyUseCase(libraryRepository)(testWork)
+        assertThat(GetWorkLocallyUseCase(libraryRepository)(testWork.id).first())
+            .isEqualTo(testWork)
+    }
+
+    @Test
+    fun saveAuthorLocallyUseCaseCorrectlySavesNewAuthor() = runBlocking {
+        val testAuthor = Author(
+            id = "TestAuthor",
+            name = "Michael Jackson",
+            birthDate = "23 janvier 2024",
+            deathDate = "10 septembre 2024",
+            wikipedia = "wikipedia.com",
+            works = flowOf(),
+            photos = flowOf()
+        )
+        SaveAuthorLocallyUseCase(libraryRepository)(testAuthor)
+        assertThat(GetAuthorLocallyUseCase(libraryRepository)(testAuthor.id).first())
+            .isEqualTo(testAuthor)
+    }
+
+    @Test
+    fun saveEditionLocallyUseCaseCorrectlySavesNewAuthor() = runBlocking {
+        val testEdition = Edition(
+            id = "testEdition",
+            title = "title",
+            isbn13 = "isbn13",
+            isbn10 = "isbn10",
+            works = flowOf(),
+            authors = flowOf(),
+            covers = flowOf(),
+        )
+        SaveEditionLocallyUseCase(libraryRepository)(testEdition)
+        assertThat(GetEditionLocallyUseCase(libraryRepository)(testEdition.id).first())
+            .isEqualTo(testEdition)
     }
 }
