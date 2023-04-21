663a8e2f3018ea7e19877c1bc53645963a13aeb6
L-roro <luis.romerorodriguez@epfl.ch>
2023-04-21 11:37:37 +0200

    Add new mocks.

--- domain/src/main/java/com/github/leuludyha/domain/model/library/Mocks.kt
+++ domain/src/main/java/com/github/leuludyha/domain/model/library/Mocks.kt
@@ -2,6 +2,7 @@ package com.github.leuludyha.domain.model.library
 
 import com.github.leuludyha.domain.model.authentication.AuthenticationContext
 import com.github.leuludyha.domain.model.user.MainUser
+import com.github.leuludyha.domain.model.user.User
 import com.github.leuludyha.domain.model.user.preferences.UserPreferences
 import com.github.leuludyha.domain.model.user.preferences.UserStatistics
 import com.github.leuludyha.domain.model.user.preferences.WorkPreference
@@ -16,8 +17,68 @@ object Mocks {
         id = "OL45804W",
         authors = flowOf(listOf()),
         editions = flowOf(listOf()),
-        covers = flowOf(listOf(6498519, 8904777, 108274, 233884, 1119236, 10222599, 10482837, 3216657, 10519563, 10835922, 10835924, 10861366, 10883671, 8760472, 12583098, 10482548, 10831929, 10835926, 12333895, 12498647, 7682784, 12143357, 12781739, 3077458).map{ Cover(it.toLong()) }),
-        subjects = flowOf(listOf("Animals", "Hunger", "Open Library Staff Picks", "Juvenile fiction", "Children's stories, English", "Foxes", "Fiction", "Zorros", "Ficci\u00f3n juvenil", "Tunnels", "Interviews", "Farmers", "Children's stories", "Rats", "Welsh Authors", "English Authors", "Thieves", "Tricksters", "Badgers", "Children's fiction", "Foxes, fiction", "Underground", "Renards", "Romans, nouvelles, etc. pour la jeunesse", "Children's literature", "Plays", "Children's plays", "Children's stories, Welsh", "Agriculteurs", "Large type books", "Fantasy fiction")),
+        covers = flowOf(
+            listOf(
+                6498519,
+                8904777,
+                108274,
+                233884,
+                1119236,
+                10222599,
+                10482837,
+                3216657,
+                10519563,
+                10835922,
+                10835924,
+                10861366,
+                10883671,
+                8760472,
+                12583098,
+                10482548,
+                10831929,
+                10835926,
+                12333895,
+                12498647,
+                7682784,
+                12143357,
+                12781739,
+                3077458
+            ).map { Cover(it.toLong()) }),
+        subjects = flowOf(
+            listOf(
+                "Animals",
+                "Hunger",
+                "Open Library Staff Picks",
+                "Juvenile fiction",
+                "Children's stories, English",
+                "Foxes",
+                "Fiction",
+                "Zorros",
+                "Ficci\u00f3n juvenil",
+                "Tunnels",
+                "Interviews",
+                "Farmers",
+                "Children's stories",
+                "Rats",
+                "Welsh Authors",
+                "English Authors",
+                "Thieves",
+                "Tricksters",
+                "Badgers",
+                "Children's fiction",
+                "Foxes, fiction",
+                "Underground",
+                "Renards",
+                "Romans, nouvelles, etc. pour la jeunesse",
+                "Children's literature",
+                "Plays",
+                "Children's plays",
+                "Children's stories, Welsh",
+                "Agriculteurs",
+                "Large type books",
+                "Fantasy fiction"
+            )
+        ),
     )
 
     val authorRoaldDahl = Author(
@@ -28,7 +89,14 @@ object Mocks {
         //bio = "Roald Dahl was a British novelist, short story writer, and screenwriter.",
         id = "OL34184A",
         works = flowOf(listOf(dumbWorkMrFox)),
-        covers = flowOf ( listOf(9395323, 9395316, 9395314, 9395313, 6287214).map { Cover(it.toLong()) } ),
+        covers = flowOf(
+            listOf(
+                9395323,
+                9395316,
+                9395314,
+                9395313,
+                6287214
+            ).map { Cover(it.toLong()) }),
     )
 
     val editionMrFox = Edition(
@@ -46,8 +114,68 @@ object Mocks {
         id = "OL45804W",
         authors = flowOf(listOf(authorRoaldDahl)),
         editions = flowOf(listOf(editionMrFox)),
-        covers = flowOf(listOf(6498519, 8904777, 108274, 233884, 1119236, 10222599, 10482837, 3216657, 10519563, 10835922, 10835924, 10861366, 10883671, 8760472, 12583098, 10482548, 10831929, 10835926, 12333895, 12498647, 7682784, 12143357, 12781739, 3077458).map{Cover(it.toLong())}),
-        subjects = flowOf(listOf("Animals", "Hunger", "Open Library Staff Picks", "Juvenile fiction", "Children's stories, English", "Foxes", "Fiction", "Zorros", "Ficci\u00f3n juvenil", "Tunnels", "Interviews", "Farmers", "Children's stories", "Rats", "Welsh Authors", "English Authors", "Thieves", "Tricksters", "Badgers", "Children's fiction", "Foxes, fiction", "Underground", "Renards", "Romans, nouvelles, etc. pour la jeunesse", "Children's literature", "Plays", "Children's plays", "Children's stories, Welsh", "Agriculteurs", "Large type books", "Fantasy fiction")),
+        covers = flowOf(
+            listOf(
+                6498519,
+                8904777,
+                108274,
+                233884,
+                1119236,
+                10222599,
+                10482837,
+                3216657,
+                10519563,
+                10835922,
+                10835924,
+                10861366,
+                10883671,
+                8760472,
+                12583098,
+                10482548,
+                10831929,
+                10835926,
+                12333895,
+                12498647,
+                7682784,
+                12143357,
+                12781739,
+                3077458
+            ).map { Cover(it.toLong()) }),
+        subjects = flowOf(
+            listOf(
+                "Animals",
+                "Hunger",
+                "Open Library Staff Picks",
+                "Juvenile fiction",
+                "Children's stories, English",
+                "Foxes",
+                "Fiction",
+                "Zorros",
+                "Ficci\u00f3n juvenil",
+                "Tunnels",
+                "Interviews",
+                "Farmers",
+                "Children's stories",
+                "Rats",
+                "Welsh Authors",
+                "English Authors",
+                "Thieves",
+                "Tricksters",
+                "Badgers",
+                "Children's fiction",
+                "Foxes, fiction",
+                "Underground",
+                "Renards",
+                "Romans, nouvelles, etc. pour la jeunesse",
+                "Children's literature",
+                "Plays",
+                "Children's plays",
+                "Children's stories, Welsh",
+                "Agriculteurs",
+                "Large type books",
+                "Fantasy fiction"
+            )
+        ),
     )
 
     private val dumbWork1984 = Work(
@@ -120,16 +248,32 @@ object Mocks {
             averageNumberOfPages = 42
         ),
         friends = listOf(),
-        latitude = 0.0,
-        longitude = 0.0
+        latitude = 46.521131,
+        longitude = 6.566055
+    )
+
+    val user2: MainUser = MainUser(
+        UUID.randomUUID().toString(),
+        username = "MockMike",
+        preferences = userPreferences,
+        phoneNumber = "",
+        profilePictureUrl = "",
+        statistics = UserStatistics(
+            preferredWorks = listOf(workLaFermeDesAnimaux),
+            preferredSubjects = listOf("Libertarianism"),
+            preferredAuthors = listOf(authorGeorgeOrwell),
+            averageNumberOfPages = 42
+        ),
+        friends = listOf(mainUser),
+        latitude = 46.522397,
+        longitude = 6.563162
     )
 
     val authContext: AuthenticationContext = AuthenticationContext(mainUser)
 
     //These are a few locations near EPFL, Pair<Latitude, Longitude>
-    val userLocationList: List<Pair<Double, Double>> = listOf(
-        Pair(46.521131, 6.566055),
-        Pair(46.522397, 6.563162),
-        Pair(46.518300, 6.560984)
+    val userList: List<User> = listOf(
+        mainUser,
+        user2
     )
 }
