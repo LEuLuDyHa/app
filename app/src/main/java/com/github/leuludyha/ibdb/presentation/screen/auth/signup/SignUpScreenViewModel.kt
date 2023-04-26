01cbabf7adf7347f096be3096aef4dc349fc6182
L-roro <luis.romerorodriguez@epfl.ch>
2023-04-26 20:39:15 +0200

    Add persistent storage to walkthrough.

--- app/src/main/java/com/github/leuludyha/ibdb/presentation/screen/auth/signup/SignUpScreenViewModel.kt
+++ app/src/main/java/com/github/leuludyha/ibdb/presentation/screen/auth/signup/SignUpScreenViewModel.kt
@@ -1,10 +1,12 @@
 package com.github.leuludyha.ibdb.presentation.screen.auth.signup
 
+import android.content.Context
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.github.leuludyha.domain.model.authentication.AuthenticationContext
+import com.github.leuludyha.ibdb.util.Constant
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
@@ -21,26 +23,37 @@ class SignUpScreenViewModel @Inject constructor(
      * Saves a boolean which indicates that the walk-through has been completed once
      * by the user. The boolean is persisted through sessions (Application restarts)
      */
-    fun rememberWalkThroughIsCompleted() {
+    fun rememberWalkThroughIsCompleted(context: Context) {
+        val sp = context.getSharedPreferences(Constant.SIGN_UP_SHARED_PREFERENCES_KEY, Context.MODE_PRIVATE)
+        val spEditor = sp.edit()
+        spEditor.putBoolean(Constant.SIGN_UP_WALKTHROUGH_COMPLETED, true)
+        spEditor.apply()
         walkThroughCompleted = true
-        return
-        TODO("Not yet implemented")
     }
 
     /**
      * @return True if the walk through was already completed, false otherwise. The result
      * is persisted through sessions (Application restarts)
      */
-    fun isWalkThroughCompleted(): Boolean {
+    fun isWalkThroughCompleted(context: Context): Boolean {
+        val sp = context.getSharedPreferences(Constant.SIGN_UP_SHARED_PREFERENCES_KEY, Context.MODE_PRIVATE)
+        walkThroughCompleted = sp.getBoolean(Constant.SIGN_UP_WALKTHROUGH_COMPLETED, false)
+
         return walkThroughCompleted
-        TODO("Not yet implemented")
     }
 
     /**
-     * Save the state of the user
+     * Saves to persistent storage the options that have been chosen during the first log into the app.
+     * This includes:
+     * - preferred username
+     * - dark theme preference
      */
-    fun persistUserState() {
-        return
-        TODO("Not yet implemented")
+    fun persistWalkthroughOptions(context: Context) {
+        //TODO: Once that is implemented, we should also add all other options that are done during the walkthrough
+        val sp = context.getSharedPreferences(Constant.SIGN_UP_SHARED_PREFERENCES_KEY, Context.MODE_PRIVATE)
+        val spEditor = sp.edit()
+        spEditor.putString(Constant.PREFERRED_USERNAME, authContext.principal.username)
+        spEditor.putBoolean(Constant.DARK_THEME_PREFERENCE, authContext.principal.preferences.darkTheme.value)
+        spEditor.apply()
     }
 }
