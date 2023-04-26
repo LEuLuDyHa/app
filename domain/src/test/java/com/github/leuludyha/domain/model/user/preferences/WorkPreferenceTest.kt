fd31d96263e988b24baf3800b85bdd565e3b9e51
Dylan <dylan.vairoli@gmail.com>
2023-04-26 19:50:41 +0200

    Improve tests for `WorkPreference`

--- domain/src/test/java/com/github/leuludyha/domain/model/user/preferences/WorkPreferenceTest.kt
+++ domain/src/test/java/com/github/leuludyha/domain/model/user/preferences/WorkPreferenceTest.kt
@@ -1,8 +1,10 @@
 package com.github.leuludyha.domain.model.user.preferences
 
+import com.github.leuludyha.domain.model.library.Mocks.editionMrFox
 import com.github.leuludyha.domain.model.library.Mocks.work1984
 import com.github.leuludyha.domain.model.library.Mocks.workMrFox
 import com.github.leuludyha.domain.model.library.Mocks.workMrFoxPref
+import com.google.common.truth.Truth
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Test
 import org.hamcrest.Matchers.`is` as Is
@@ -12,11 +14,13 @@ class WorkPreferenceTest {
     @Test
     fun fieldsAreProperlyAccessed() {
         assertThat(workMrFoxPref, Is(workMrFoxPref))
+        Truth.assertThat(workMrFoxPref).isNotEqualTo(editionMrFox)
+        Truth.assertThat(workMrFoxPref).isNotEqualTo(work1984)
         assertThat(workMrFoxPref.Id(), Is(workMrFoxPref.work.id))
         assertThat(workMrFoxPref.work, Is(workMrFox))
         assertThat(workMrFoxPref.readingState, Is(WorkPreference.ReadingState.READING))
         assertThat(workMrFoxPref.possessed, Is(true))
-        assertThat(workMrFoxPref.rating, Is(null))
+        Truth.assertThat(workMrFoxPref.rating).isNull()
     }
 
     @Test
